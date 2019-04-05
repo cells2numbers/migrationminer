021d7290ab1fe2db68925049f9e98bf51ae0362b
Tim Becker <tbecker@broadinstitute.org>
2019-04-05 14:46:04 -0400

    added windrose plot

--- R/track.R
+++ R/track.R
@@ -637,6 +637,133 @@ summarize_sectors <- function(tracks, strata) {
         sector_down  / (sector_left + sector_right + sector_up + sector_down))
 }
 
+
+#'Windrose style plot
+#'
+#' @param tracks data frame with track objects
+#' @param spedres speed resolution used for coloring
+#' @param dirres angle resolution of the plot
+#' @param spdmin minimum speed (used for color mapping of speed)
+#' @param spdmax max speed (used for color mapping of speed )
+#' @param palette color map (used for color mapping of speed)
+#' @param title_name title name of the plot
+#' @param scale_name legend name / title for the scale
+#' @return windrose_plot
+#'
+#' @importFrom magrittr %>% %<>%
+#' @export
+plot_windrose <- function(tracks,
+  spdres = 1,
+  dirres = 30,
+  spdmin = 0,
+  spdmax = 5,
+  palette = "YlGnBu",
+  scale_name = "speed in pixel/frame",
+  title_name = "Distribution of angle and speed"){
+
+  tracks %<>%
+    mutate(Track_Angle = Track_Angle + pi/2) %>% # rotate all angles by 90 degree or pi/2
+    mutate(Track_Angle = ifelse(Track_Angle > pi, Track_Angle - 2*pi, Track_Angle) ) %>%
+    mutate(Track_Angle = ifelse(Track_Angle < 0, Track_Angle + 2*pi, Track_Angle) ) %>%
+    filter(!is.na(Track_Speed), !is.na(Track_Directionality))
+
+
+  data <- data.frame(
+    spd = tracks$Track_Speed ,
+    dir = (180 * (tracks$Track_Angle) / pi)
+  )
+
+  spd = "spd"
+  dir = "dir"
+
+
+  # Tidy up input data ----
+  n.in <- NROW(data)
+  dnu <- (is.na(data[[spd]]) | is.na(data[[dir]]))
+  data[[spd]][dnu] <- NA
+  data[[dir]][dnu] <- NA
+
+  # figure out the wind speed bins ----
+  spdseq <- seq(spdmin,spdmax,spdres)
+
+  # get some information about the number of bins, etc.
+  n.spd.seq <- length(spdseq)
+  n.colors.in.range <- n.spd.seq - 1
+
+  # create the color map
+  spd.colors <- colorRampPalette(brewer.pal(min(max(3,
+    n.colors.in.range),
+    min(9,
+      n.colors.in.range)),
+    palette))(n.colors.in.range)
+
+  if (max(data[[spd]],na.rm = TRUE) > spdmax) {
+    spd.breaks <- c(spdseq,
+      max(data[[spd]],na.rm = TRUE))
+    spd.labels <- c(paste(c(spdseq[1:n.spd.seq - 1]),
+      '-',
+      c(spdseq[2:n.spd.seq])),
+      paste(spdmax,
+        "-",
+        max(data[[spd]],na.rm = TRUE)))
+    spd.colors <- c(spd.colors, "grey50")
+  } else{
+    spd.breaks <- spdseq
+    spd.labels <- paste(c(spdseq[1:n.spd.seq - 1]),
+      '-',
+      c(spdseq[2:n.spd.seq]))
+  }
+  data$spd.binned <- cut(x = data[[spd]],
+    breaks = spd.breaks,
+    labels = spd.labels,
+    ordered_result = TRUE)
+  # clean up the data
+  data. <- na.omit(data)
+
+  # figure out the wind direction bins
+  dir.breaks <- c(-dirres/2,
+    seq(dirres/2, 360 - dirres/2, by = dirres),
+    360 + dirres/2)
+  #
+  dir.labels <- c(paste(360 - dirres/2,"-",dirres/2),
+    paste(seq(dirres/2, 360 - 3*dirres/2, by = dirres),
+      "-",
+      seq(3*dirres/2, 360 - dirres/2, by = dirres)),
+    paste(360 - dirres/2,"-",dirres/2))
+
+  # assign each wind direction to a bin
+  dir.binned <- cut(data[[dir]],
+    breaks = dir.breaks,
+    ordered_result = TRUE)
+  levels(dir.binned) <- dir.labels
+  data$dir.binned <- dir.binned
+
+
+  # deal with change in ordering introduced somewhere around version 2.2
+  if (packageVersion("ggplot2") > "2.2") {
+    #cat("Hadley broke my code\n")
+    data$spd.binned = with(data, factor(spd.binned, levels = rev(levels(spd.binned))))
+    spd.colors = rev(spd.colors)
+  }
+
+  # create the plot ----
+  windrose_plot <- ggplot(data = data,
+    aes(x = dir.binned,
+      fill = spd.binned)) +
+    geom_bar() +
+    scale_x_discrete(drop = FALSE,
+      labels = waiver()) +
+    coord_polar(start = -((dirres/2)/360) * 2*pi) +
+    scale_fill_manual(name = scale_name,
+      values = spd.colors,
+      drop = FALSE) +
+    theme(axis.title.x = element_blank()) +
+    labs(title = title_name)
+
+  # return the handle to the wind rose
+  return(windrose_plot)
+}
+
 #'3D trajectory plot
 #'
 #' @param tracks data frame with track objects
